b3feea19535a65622e34ff0a028f0d593dad7889
Alie Stone <helloaliestone@gmail.com>
2020-10-29 13:42:17 +0000

    Automatically backed up by Learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,3 +1,4 @@
+
 require 'spec_helper'
 
 describe "GET '/' - Greeting Form" do
@@ -6,6 +7,15 @@ describe "GET '/' - Greeting Form" do
     visit '/'
     expect(page.body).to include("Welcome!")
   end
+
+  # New test
+  it 'has a greeting form with a user_name field' do
+    visit '/'
+
+    expect(page).to have_selector("form")
+    expect(page).to have_field(:user_name)
+  end
+end
 
 describe "POST '/greet' - User Greeting" do
   it 'greets the user personally based on their user_name in the form' do
@@ -17,4 +27,3 @@ describe "POST '/greet' - User Greeting" do
     expect(page).to have_text("Hi Avi, nice to meet you!")
   end
 end
-end 
